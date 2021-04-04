b61b9050aad45aff2ba2e17bdba311ee900bbdaf
AleksandrKolesnikov1997 <81776849+AleksandrKolesnikov1997@users.noreply.github.com>
2021-04-04 13:54:48 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,10 @@
-# a = int(input('А = '))
-# b = float(input('B = '))
-# c = a * b
-# print('C = a * b')
-# print('C =', c)
-# d = input('Введите слово :')
-# e = bool(input('False or True'))
-# edd_list = [a, b, c, d, e]
-# for i in range(len(edd_list)):
-#     print(edd_list[i], type(edd_list[i]))
+a = int(input('А = '))
+b = float(input('B = '))
+c = a * b
+print('C = a * b')
+print('C =', c)
+d = input('Введите слово :')
+e = bool(input('False or True'))
+edd_list = [a, b, c, d, e]
+for i in range(len(edd_list)):
+    print(edd_list[i], type(edd_list[i]))
